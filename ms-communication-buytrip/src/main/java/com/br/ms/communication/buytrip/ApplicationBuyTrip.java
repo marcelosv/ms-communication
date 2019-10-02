ee69dc2b95d0f296203075d732283aaa00a0e6a5
Marcelo Vieira <marcelosouzav@gmail.com>
2019-10-02 06:36:46 -0300

    finalizando curso

--- ms-communication-buytrip/src/main/java/com/br/ms/communication/buytrip/ApplicationBuyTrip.java
+++ ms-communication-buytrip/src/main/java/com/br/ms/communication/buytrip/ApplicationBuyTrip.java
@@ -2,10 +2,8 @@ package com.br.ms.communication.buytrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ApplicationBuyTrip {
 
     public static void main(String[] args) {
--- ms-communication-gateway/src/main/java/com/br/ms/communication/gateway/ApplicationGateway.java
+++ ms-communication-gateway/src/main/java/com/br/ms/communication/gateway/ApplicationGateway.java
@@ -2,15 +2,13 @@ package com.br.ms.communication.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableZuulProxy
 public class ApplicationGateway {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(ApplicationGateway.class, args);
     }
 }
